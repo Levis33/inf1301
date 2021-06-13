4f91e434742343a46126a7590838df8bb36547ca
Raphael Fleury <raphael.fleury@b2wdigital.com>
2021-06-13 10:09:18 -0300

    testes unitarios

--- teste.py
+++ teste.py
@@ -1,5 +1,7 @@
 #Thiago Levis Alambert Rodrigues
 import tkinter
+import sys
+import os
 from model import game_rules
 from view import draw_canvas
 
@@ -45,7 +47,34 @@ def desenhaEscolhaNivel():
     cnv = tkinter.Canvas(root, bg="white", height=700, width=1200)
     draw_canvas.escolheNivel(cnv)
     cnv.pack()
-    root.mainloop() 
+    root.mainloop()
+
+def guardaJogadas():
+    arq = open("teste.txt","a")
+    print(game_rules.tentativaSenha, game_rules.respostaSenha)
+    tentativaGuarda = ""
+    respostaGuarda = ""
+    for i in range(0,len(game_rules.tentativaSenha)):
+        tentativaGuarda += str(game_rules.tentativaSenha[i])+ ' '
+        respostaGuarda += str(game_rules.respostaSenha[i]) + ' '
+    arq.writelines("%s%s\n" %(tentativaGuarda,respostaGuarda))
+    print(arq)
+    arq.close()
+
+
+def inicializaJogoGuardado():
+  arq = open("teste.txt","r")
+  game_rules.senha = arq.readline().strip().split(' ') #coloca a senha do arquivo txt na senha global
+  game_rules.nivel = arq.readline().strip() #coloca o nivel
+  game_rules.setDificuldade()
+  print(arq)
+  arq.close()
+
+
+def restartGame():
+  python = sys.executable
+  os.execl(python, python, * sys.argv)
+  print("jogo foi restartado")  
 
 
 # Chamadas dos testes
@@ -55,4 +84,7 @@ def desenhaEscolhaNivel():
 # testeCompara()
 # desenhaTabuleiro()
 # desenhaEscolhaNivel()
+# guardaJogadas()
+# inicializaJogoGuardado()
+# restartGame()
 
